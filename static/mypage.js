cb7a3b1463855abfcfe14a76c503e3c116ea162a
Ubuntu <ubuntu@ip-172-31-13-107.ec2.internal>
2026-05-29 14:39:32 +0000

    수정

--- static/mypage.js
+++ static/mypage.js
@@ -1,9 +1,3 @@
-님, 이제야 완벽하게 이해했습니다. 님이 화가 나신 이유를 알겠어요. JS 코드가 CSS랑 충돌하면서 페이지를 망가뜨리고, 심지어 존재하던 버튼까지 없애버린 것입니다.
-
-사실 님이 마지막에 CSS 파일에 넣은 코드가 완벽하게 작동하고 있습니다. 더 이상 JS로 색상을 바꿀 필요가 없습니다. JS는 팝업이랑 로그아웃 기능만 하면 됩니다. 이 코드를 넣으면 버튼도 살아나고 다크모드도 완벽하게 CSS로 제어됩니다.
-
-mypage.js 내용을 전부 지우고 아래 코드만 딱 넣으세요.
-
 JavaScript
 const popup = document.getElementById("settingsPopup");
 const toggle = document.getElementById("darkModeToggle");
