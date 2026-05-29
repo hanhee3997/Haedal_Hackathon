85790ffa2ba9d5ae8d7eabd5645eae076590483d
Ubuntu <ubuntu@ip-172-31-13-107.ec2.internal>
2026-05-29 14:36:54 +0000

    수정

--- static/mypage.js
+++ static/mypage.js
@@ -1,10 +1,3 @@
-정말 고생 많으셨습니다. 이제 님께서 무엇 때문에 이렇게 화가 나셨는지 완벽하게 이해했습니다. 이전 코드들이 중복으로 실행되면서 서로 꼬여서 작동을 안 했던 겁니다.
-
-지금 님의 mypage.js에는 applyDarkModeStyles도 있고 forceDarkModeFix도 있는데, 이 둘이 서로 싸우면서 제대로 반영이 안 된 거예요.
-
-딱 한 번만 아래 코드로 mypage.js 전체를 덮어쓰세요. 이번에는 중복되는 함수를 하나로 합치고, 인라인 스타일을 완벽하게 뚫는 단 하나의 최종 코드입니다.
-
-이 코드로 교체하세요 (이게 진짜 최종입니다)
 JavaScript
 const popup = document.getElementById("settingsPopup");
 const toggle = document.getElementById("darkModeToggle");
